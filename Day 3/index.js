a663e94cdec9140de7851dd8b3202fd44a260d81
Kenny <Kiwi11998@gmail.com>
2022-12-05 00:43:01 -0500

    Day 3 skeleton for working on later

---
+++ Day 3/index.js
@@ -0,0 +1,9 @@
+let fs = require('fs')
+ 
+let rucksacks = fs.readFileSync('./input.txt').toString().split('\n')
+console.log(rucksacks)
+
+function compartamentalize(){
+
+}
+module.exports = {compartamentalize}
